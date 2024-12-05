0b9d54f249f8e8434b481dcac8a5f3d14ae44d09
neneee <2022180024@tukorea.ac.kr>
2024-12-05 22:18:28 +0900

    bug : 구조는 완성, 모드 변경 시, 오류

--- KarKartrider/KarKartrider/Mode.h
+++ KarKartrider/KarKartrider/Mode.h
@@ -3,6 +3,16 @@
 
 class Mode {
 public:
+
+	void init() {
+		// play mode에 필요한 모델 로드
+		loadModelWithProgress <KartModel>("kronos.obj", "obj/car/kronos/", "car", "sphere", glm::scale(glm::mat4(1.0f), glm::vec3(10.0, 10.0, 10.0)), karts);
+		loadModelWithProgress <RoadModel>("road_all_1.obj", "obj/road/", "road_all", "cube", glm::scale(glm::mat4(1.0f), glm::vec3(20.0, 20.0, 20.0)), roads);
+
+		initializeModelsWithPhysics(karts); // 모든 모델 Bullet world에 추가
+		initializeModelsWithPhysics(roads);
+	}
+
 	virtual void init() = 0; // 모드 시작시 셋팅
 	virtual void keyboard() = 0;
 	virtual void draw_model() = 0;
